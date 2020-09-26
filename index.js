dd5d02dc3402d87af01ac59505497c75ede8c087
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-26 00:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
 
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
